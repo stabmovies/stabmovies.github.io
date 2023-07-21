595689c8ef64d1e82015e2d5393f8770766177e2
stabmovies <139116309+stabmovies@users.noreply.github.com>
2023-07-21 11:27:58 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const logo = document.querySelector(".logo");
+  
+    // Fade in the logo after a short delay (you can adjust the delay as needed)
+    setTimeout(() => {
+      logo.style.opacity = "1";
+    }, 1000);
+  });
